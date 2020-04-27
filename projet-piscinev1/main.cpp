574a450e47e6017e5fc887c05b6eeb0da762ec68
sintyx89 <43741356+sintyx89@users.noreply.github.com>
2020-04-27 08:01:46 +0200

    structure du menu
    aujout de la structure du menu pour le projet.
    saisie blindé et autilisation du switch pour la décision.

--- projet-piscinev1/main.cpp
+++ projet-piscinev1/main.cpp
@@ -15,5 +15,48 @@ int main()
                 << "les classes sont a completer et le chargement depuis un fichier a implementer" 
                 << std::endl;
 
+    /// menu du projet
+    int choix = 0;
+
+    std::cout   << "saisir votre choix : "
+                << std::endl
+                << "1- chargement du graphe"
+                << std::endl
+                << "2- chargement des ponderations"
+                << std::endl
+                << "3- calcule des indices"
+                << std::endl
+                << "4- affichage du graphe"
+                << std::endl
+                << "5- autre"
+                << std::endl;
+
+    while (choix < 1 || choix > 5)
+    {
+        std::cin    >> choix;
+    }
+    
+    switch (choix)
+    {
+    case 1:
+        // chargement du graphe
+        break;
+    case 2:
+        // chargement des ponderations
+        break;
+    case 3:
+        // calcule des indices
+        break;
+    case 4:
+        //  affichage du graphe
+        break;
+    case 5:
+        // autre
+        break;
+    default:
+        break;
+    }
+
+
     return 0;
 }
